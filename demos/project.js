37f951985786a590ad97fffa9e6b8a977cf8d7c5
SteveMan67 <poppt@acatulsa.org>
2026-01-09 17:30:03 -0600

    updated project json into a seperate file

--- demos/project.js
+++ demos/project.js
@@ -1,26 +1,17 @@
-let projects = [
-  {
-    "id": "mouse-border-glow",
-    "title": "Mouse Border Glow",
-    "description": "A very polished feeling hover effect.",
-    "source": "https://codepen.io/Hyperplexed/pen/MWQeYLW?editors=0010",
-    "path": "../demos/mouse-border-glow/index.html",
-    "type": "hover"
-  }
-]
+fetch('projects.JSON')
+  .then(response => response.json())
+  .then(projects => {
+    console.log(projects)
+    let iframe = document.getElementById("project-iframe")
+    const title = document.getElementById("title")
+    const description = document.getElementById("description")
+    const projectId = new URLSearchParams(window.location.search).get("id")
 
-console.log(projects)
+    const jsonIndexOfProject = projects.findIndex(f => f.id == projectId)
+    console.log(jsonIndexOfProject)
 
-document.addEventListener('DOMContentLoaded', () => {
-  let iframe = document.getElementById("project-iframe")
-  const title = document.getElementById("title")
-  const description = document.getElementById("desctiption")
-  const projectId = new URLSearchParams(window.location.search).get("id")
-
-  const jsonIndexOfProject = projects.findIndex(f => f.id == projectId)
-
-  if (jsonIndexOfProject !== -1) {
-    iframe.src = projects[jsonIndexOfProject].path
-  }
-})
+    if (jsonIndexOfProject !== -1) {
+      iframe.src = projects[jsonIndexOfProject].path
+      }
+  })
 
