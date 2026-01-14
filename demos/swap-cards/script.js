c93184f65366e498c11ee1eafa70e10292e40f30
SteveMan67 <143290011+SteveMan67@users.noreply.github.com>
2026-01-14 16:38:55 +0000

    swipe on swap cards

--- demos/swap-cards/script.js
+++ demos/swap-cards/script.js
@@ -1,71 +1,101 @@
-const cardContainer = document.querySelector(".cards")
-let cards = document.querySelectorAll(".card:not(.removing)")
-let rightbutton = document.querySelector(".button-left")
-let leftbutton = document.querySelector(".button-right")
+const cardContainer = document.querySelector(".cards");
+let cards = document.querySelectorAll(".card:not(.removing)");
+let rightbutton = document.querySelector(".button-left");
+let leftbutton = document.querySelector(".button-right");
+const body = document.querySelector("body")
+let touchstart, touchend;
+
+body.addEventListener("touchmove", (e) => {
+  e.preventDefault()
+})
+
+cardContainer.addEventListener("touchmove", (e) => {
+  e.preventDefault()
+})
+
+cardContainer.addEventListener("touchstart", (e) => {
+  touchstart = e.touches[0].clientX;
+});
+
+cardContainer.addEventListener("touchend", (e) => {
+  touchend = e.changedTouches[0].clientX;
+  console.log(touchstart, touchend)
+  if (touchend > touchstart + 50) {
+    // swiped left
+    cards = document.querySelectorAll(".card:not(.removing)")
+    updateCardPositions("left")
+  } else if (touchend < touchstart - 50) {
+    // swiped right
+    cards = document.querySelectorAll(".card:not(.removing)")
+    updateCardPositions("right")
+  }
+});
 
 function addClickListener(card, direction) {
   card.addEventListener("click", (e) => {
-    e.preventDefault()
-    cards = document.querySelectorAll(".card:not(.removing)")
-    updateCardPositions(direction)
-  })
+    e.preventDefault();
+    cards = document.querySelectorAll(".card:not(.removing)");
+    updateCardPositions(direction);
+  });
 }
 
 function createCard(left, card) {
-  let newCard = card.cloneNode(true)
-  newCard.style.transform = left ? "translateX(calc(-50% - 1200px)) scale(0.7)" : "translateX(calc(-50% + 1200px)) scale(0.7)"
-  newCard.dataset.position = left ? "left" : "right"
-  addClickListener(newCard, newCard.dataset.position)
-  cardContainer.appendChild(newCard)
-  
+  let newCard = card.cloneNode(true);
+  newCard.style.transform = left
+    ? "translateX(calc(-50% - 1200px)) scale(0.7)"
+    : "translateX(calc(-50% + 1200px)) scale(0.7)";
+  newCard.dataset.position = left ? "left" : "right";
+  addClickListener(newCard, newCard.dataset.position);
+  cardContainer.appendChild(newCard);
+
   setTimeout(() => {
-    newCard.style.removeProperty("transform")
-  }, 10)
+    newCard.style.removeProperty("transform");
+  }, 10);
 }
 
 function updateCardPositions(position) {
   if (position == "left") {
-    let swapCard
+    let swapCard;
     for (let subcard of cards) {
       if (subcard.dataset.position == "left") {
-        subcard.dataset.position = "center"
+        subcard.dataset.position = "center";
       } else if (subcard.dataset.position == "center") {
-        subcard.dataset.position = "right"
+        subcard.dataset.position = "right";
       } else {
-        swapCard = subcard
+        swapCard = subcard;
       }
     }
-    createCard(true, swapCard)
-    swapCard.style.transform = "translateX(calc(-50% + 1200px)) scale(0.7)"
-    swapCard.classList.add("removing")
+    createCard(true, swapCard);
+    swapCard.style.transform = "translateX(calc(-50% + 1200px)) scale(0.7)";
+    swapCard.classList.add("removing");
 
     setTimeout(() => {
-      swapCard.remove()
-    }, 1000)
+      swapCard.remove();
+    }, 1000);
   } else if (position == "right") {
-    let swapCard
+    let swapCard;
     for (subcard of cards) {
       if (subcard.dataset.position === "right") {
-        subcard.dataset.position = "center"
+        subcard.dataset.position = "center";
       } else if (subcard.dataset.position === "center") {
-        subcard.dataset.position = "left"
+        subcard.dataset.position = "left";
       } else {
-        swapCard = subcard
+        swapCard = subcard;
       }
     }
-    createCard(false, swapCard)
-    swapCard.style.transform = "translateX(calc(-50% - 1200px)) scale(0.7)" 
-    swapCard.classList.add("removing")
+    createCard(false, swapCard);
+    swapCard.style.transform = "translateX(calc(-50% - 1200px)) scale(0.7)";
+    swapCard.classList.add("removing");
 
     setTimeout(() => {
-      swapCard.remove()
-    }, 1000)
+      swapCard.remove();
+    }, 1000);
   }
-} 
-
-for (const card of cards) {
-  addClickListener(card, card.dataset.position)
 }
 
-addClickListener(rightbutton, "left")
-addClickListener(leftbutton, "right")
+for (const card of cards) {
+  addClickListener(card, card.dataset.position);
+}
+
+addClickListener(rightbutton, "right");
+addClickListener(leftbutton, "left");
