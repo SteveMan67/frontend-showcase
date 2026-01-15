9abc7bee94695e0096d66e12810d6975ebd26de3
SteveMan67 <poppt@acatulsa.org>
2026-01-15 11:17:38 -0600

    more cats broke swipe

--- demos/swap-cards/script.js
+++ demos/swap-cards/script.js
@@ -6,12 +6,35 @@ const body = document.querySelector("body")
 let touchstart, touchend;
 
 let diff
+let mouseDown = false
+let visibleCards = []
+let cardImages = ["https://images.unsplash.com/photo-1573865526739-10659fec78a5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2F0fGVufDB8fDB8fHww", "https://images.unsplash.com/photo-1495360010541-f48722b34f7d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2F0fGVufDB8fDB8fHww", "https://images.unsplash.com/photo-1533738363-b7f9aef128ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNhdHxlbnwwfHwwfHx8MA%3D%3D", "https://images.unsplash.com/photo-1571566882372-1598d88abd90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGNhdHxlbnwwfHwwfHx8MA%3D%3D", "https://images.unsplash.com/photo-1511044568932-338cba0ad803?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGNhdHxlbnwwfHwwfHx8MA%3D%3D"]
+let cardList = []
+for (card in cardImages) {
+  console.log(card)
+  let div = document.createElement("div")
+  div.classList.add("card")
+  div.innerHTML = `<img src="${cardImages[card]}">`
+  if (card == 0) {
+    div.dataset.position = "left"
+    visibleCards.push(div)
+  } else if (card == 1) {
+    div.dataset.position = "center"
+    visibleCards.push(div)
+  } else if (card == 2) {
+    div.dataset.position = "right"
+    visibleCards.push(div)
+  } else {
+    div.dataset.position = "hidden"
+  }
+  cardContainer.appendChild(div)
+  cardList.push(div)
+}
+console.log(cardList)
 
-cardContainer.addEventListener("touchmove", (e) => {
-  e.preventDefault()
-  const currentX = e.touches[0].clientX;
+function touchMoveLogic(clientX) {
+  const currentX = clientX
   diff = currentX - touchstart
-
   cards.forEach(card => {
     if (card.dataset.position == "center") {
       let scale 
@@ -24,7 +47,9 @@ cardContainer.addEventListener("touchmove", (e) => {
       } else {
         scale = 0.6
       }
+
       card.style.transform = `translateX(calc(-50% + ${diff}px)) scale(${scale})`
+    
     } else if (card.dataset.position == "left") {
       let scale
       if (diff > 0 && diff < 1000) {
@@ -52,17 +77,19 @@ cardContainer.addEventListener("touchmove", (e) => {
       card.style.transform = `translateX(calc(-50% + 500px + ${diff}px)) scale(${scale})`
     }
   })
-})
 
-cardContainer.addEventListener("touchstart", (e) => {
-  touchstart = e.touches[0].clientX;
+  // add new cards and remove old ones
+}
+
+function touchStartLogic(clientX) {
+  touchstart = clientX;
   touchStartTime = Date.now()
   cards = document.querySelectorAll(".card:not(.removing)")
   cards.forEach(card => card.style.transition = "none")
-});
+}
 
-cardContainer.addEventListener("touchend", (e) => {
-  touchend = e.changedTouches[0].clientX;
+function touchEndLogic (clientX) {
+  touchend = clientX;
   cards = document.querySelectorAll(".card:not(.removing)")
   cards.forEach(card => {
     card.style.transition = ''
@@ -71,7 +98,55 @@ cardContainer.addEventListener("touchend", (e) => {
   if (Math.abs(diff) > 50) {
     updateCardPositions(diff > 0 ? "left" : "right")
   }
+}
+
+cardContainer.addEventListener("touchmove", (e) => {
+  e.preventDefault()
+  touchMoveLogic(e.touches[0].clientX)
+})
+
+cardContainer.addEventListener("mousemove", (e) => {
+  if (mouseDown) {
+    touchMoveLogic(e.clientX)
+  }
+})
+
+cardContainer.addEventListener("touchstart", (e) => {
+  touchStartLogic(e.touches[0].clientX)
 });
+
+cardContainer.addEventListener("mousedown", (e) => {
+  e.preventDefault()
+  mouseDown = true
+  touchStartLogic(e.clientX)
+})
+
+cardContainer.addEventListener("touchend", (e) => {
+  touchEndLogic(e.changedTouches[0].clientX)
+});
+
+cardContainer.addEventListener("mouseup", (e) => {
+  mouseDown = false
+  touchEndLogic(e.clientX)
+})
+
+function findNextCard(left, card) {
+  if (left) {
+    let index = cardList.indexOf(card)
+    if (index == 0) {
+      return cardList[cardList.length - 1]
+    } else {
+      return cardList[index - 1]
+    }
+  } else {
+    let index = cardList.indexOf(card)
+    if (index == cardList.length - 1) {
+      return cardList[0]
+    } else {
+      return cardList[index + 1]
+    }
+  }
+}
 
 function addClickListener(card, direction) {
   card.addEventListener("click", (e) => {
@@ -81,56 +156,60 @@ function addClickListener(card, direction) {
   });
 }
 
-function createCard(left, card) {
-  let newCard = card.cloneNode(true);
-  newCard.style.transform = left
+function addCard(left, card, oldCard) {
+  if (left) {
+    visibleCards.unshift(card)
+    visibleCards.pop()
+  } else {
+    visibleCards.push(card)
+    visibleCards.shift()
+  }
+  card.style.transition = "none"
+  card.dataset.position = left ? "left" : "right"
+  card.style.transform = left
     ? "translateX(calc(-50% - 1200px)) scale(0.7)"
     : "translateX(calc(-50% + 1200px)) scale(0.7)";
-  newCard.dataset.position = left ? "left" : "right";
-  cardContainer.appendChild(newCard);
-
+  oldCard.style.transform = left ? "translateX(calc(-50% + 1200px)) scale(0.7)" : "translateX(calc(-50% - 1200px)) scale(0.7)"
+  void card.offsetWidth
+  card.style.transition = ""
   setTimeout(() => {
-    newCard.style.removeProperty("transform");
+    card.style.removeProperty("transform");
   }, 10);
 }
 
 function updateCardPositions(position) {
   if (position == "left") {
-    let swapCard;
-    for (let subcard of cards) {
-      if (subcard.dataset.position == "left") {
+    let newCard;
+    let removedCard
+    for (let subcard of cardList) {
+      if (subcard.dataset.position == "hidden") {
+
+      } else if (subcard.dataset.position == "left") {
         subcard.dataset.position = "center";
+        newCard = findNextCard(true, subcard)
       } else if (subcard.dataset.position == "center") {
         subcard.dataset.position = "right";
-      } else {
-        swapCard = subcard;
+      } else if (subcard.dataset.position == "right") {
+        removedCard = subcard
+        subcard.dataset.position = "hidden"
       }
     }
-    createCard(true, swapCard);
-    swapCard.style.transform = "translateX(calc(-50% + 1200px)) scale(0.7)";
-    swapCard.classList.add("removing");
-
-    setTimeout(() => {
-      swapCard.remove();
-    }, 1000);
+    addCard(true, newCard, removedCard);
   } else if (position == "right") {
-    let swapCard;
-    for (subcard of cards) {
+    let newCard;
+    let removedCard
+    for (subcard of cardList) {
       if (subcard.dataset.position === "right") {
         subcard.dataset.position = "center";
+        newCard = findNextCard(false, subcard)
       } else if (subcard.dataset.position === "center") {
         subcard.dataset.position = "left";
-      } else {
-        swapCard = subcard;
+      } else if (subcard.dataset.position == "left") {
+        removedCard = subcard
+        subcard.dataset.position = "hidden"
       }
     }
-    createCard(false, swapCard);
-    swapCard.style.transform = "translateX(calc(-50% - 1200px)) scale(0.7)";
-    swapCard.classList.add("removing");
-
-    setTimeout(() => {
-      swapCard.remove();
-    }, 1000);
+    addCard(false, newCard, removedCard);
   }
 }
 
