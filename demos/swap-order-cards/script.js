019b554620f337d69ae194fa0e816c511b2e5da3
Timothy Popp <poppt@acatulsa.org>
2026-01-19 12:28:05 -0600

    logic.

--- demos/swap-order-cards/script.js
+++ demos/swap-order-cards/script.js
@@ -1,35 +1,52 @@
 const cardContainer = document.querySelector(".card-container")
 const dragContainer = document.querySelector(".dragging")
+const selectedContainer = document.querySelector(".selected-area")
 let cards = document.querySelectorAll(".card")
-console.log(cards)
 let mouseDown = false
 let offsetX, offsetY
 let isUpdating
+let dragCard = null
+
+function insertAt(card, index) {
+  const ref = cardContainer.children[index] || null
+  cardContainer.insertBefore(card, ref)
+}
 
 cards.forEach(card => {
   card.addEventListener("mousedown", (e) => {
-    console.log("mousedown")
     e.preventDefault()
     mouseDown = true
     // move it to the container for dragging
     offsetX = e.offsetX
     offsetY = e.offsetY
-    const rect = card.getBoundingClientRect()
+    card.classList.add("dragging-card")
     card.style.transform = `translateX(${e.clientX - offsetX}px) translateY(${e.clientY - offsetY}px)`
+    insertAt(selectedContainer, Array.from(cardContainer.querySelectorAll('.card')).indexOf(card))
+    selectedContainer.style.display = "block"
     dragContainer.appendChild(card)
+    dragCard = card
   })
   card.addEventListener("mousemove", (e) => {
-    if (mouseDown) {      
+    if (mouseDown) {
       if (!isUpdating) {
         requestAnimationFrame(() => {
-          dragCard = document.querySelector(".dragging .card")
+          if (!mouseDown || !dragCard) {
+            isUpdating = false
+            return
+          }
           e.preventDefault()
-          const rect = dragCard.getBoundingClientRect()
           dragCard.style.transform = `translateX(${e.clientX - offsetX}px) translateY(${e.clientY - offsetY}px)`
           isUpdating = false
         })
         isUpdating = true
       }
+      if (card.contains(e.target)) {
+        // move the target selector to the current position
+        insertAt(selectedContainer, Array.from(cardContainer.querySelectorAll('.card')).indexOf(card))
+        console.log(Array.from(cardContainer.querySelectorAll('.card')).indexOf(card))
+      }
+    } else {
+      card.style.transform = '';
     }
   })
 })
@@ -37,7 +54,7 @@ cards.forEach(card => {
 
 document.addEventListener('mouseup', () => {
   mouseDown = false
-  const draggingCards = document.querySelectorAll(".dragging .card")
+  const draggingCards = document.querySelectorAll(".dragging-card")
   cards = document.querySelectorAll(".card")
   cards.forEach(card => {
     card.style.transform = ''
@@ -45,5 +62,9 @@ document.addEventListener('mouseup', () => {
   draggingCards.forEach(card => {
     cardContainer.appendChild(card)
     card.style.transform = ''
+    card.classList.remove("dragging-card")
   })
+  selectedContainer.style.display = "none"
+  cardContainer.append(selectedContainer)
+  dragCard = null
 })
